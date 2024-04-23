35a4635a8e6e3a9b1170cc7f03d22eb88e1bb166
Philipp Rudiger <prudiger@anaconda.com>
2024-04-23 18:47:11 +0200

    Updates

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import datetime as dt
 import pathlib
 
 from functools import partial
+from types import FunctionType, MethodType
 from urllib.parse import urljoin
 
 from fastapi import APIRouter, FastAPI, Request, WebSocket
@@ -52,8 +53,7 @@ class DocRouter:
             if context.io_loop is None:
                 context._loop = IOLoop.current()
 
-    async def _get_session(self, app: str, request: Request,
-                           session_id: str | None) -> ServerSession:
+    async def _get_session(self, app: str, request: Request, session_id: str | None) -> ServerSession:
         if session_id is None:
             session_id = generate_session_id(secret_key=None, signed=False)
         payload = {
@@ -67,6 +67,9 @@ class DocRouter:
             expiration=300,
             extra_payload=payload
         )
+        request.protocol = 'http'
+        request.host = request.client.host
+        request.uri = request.url.path
         session = await self.application_contexts[f'/{app}'].create_session_if_needed(
             session_id, request, token
         )
@@ -75,14 +78,13 @@ class DocRouter:
     def resources(self, absolute_url: str | None = None) -> Resources:
         mode = settings.resources(default="server")
         if mode == "server":
-            root_url = urljoin(absolute_url,
-                               self._prefix) if absolute_url else self._prefix
-            return Resources(mode="server", root_url=root_url,
-                             path_versioner=StaticHandler.append_version)
+            root_url = urljoin(absolute_url, self._prefix) if absolute_url else self._prefix
+            return Resources(mode="server", root_url=root_url, path_versioner=StaticHandler.append_version)
         return Resources(mode=mode)
 
-    async def get(self, app: str, request: Request,
-                  bokeh_session_id: str | None = None) -> HTMLResponse:
+    async def get(self, app: str, request: Request, bokeh_session_id: str | None = None) -> HTMLResponse:
+        if f'/{app}' not in self.application_contexts:
+            return
         session = await self._get_session(app, request, bokeh_session_id)
         page = server_html_page_for_session(
             session,
@@ -101,8 +103,7 @@ class DocRouter:
             subprotocol = None
         if subprotocol != 'bokeh' or token is None:
             websocket.close()
-            raise RuntimeError(
-                "Subprotocol header is not 'bokeh' or token not provided")
+            raise RuntimeError("Subprotocol header is not 'bokeh' or token not provided")
 
         now = calendar.timegm(dt.datetime.now(tz=dt.timezone.utc).timetuple())
         payload = get_token_payload(token)
@@ -117,15 +118,24 @@ class DocRouter:
         self._websocket = websocket
 
         context = self.application_contexts[f'/{app}']
-
-        # log.debug("ProtocolHandler created for %r", protocol)
+        
+        #log.debug("ProtocolHandler created for %r", protocol)
 
         await websocket_handler(websocket, context, token)
 
 
-class FastAPIServerConnection(ServerConnection):
+class SocketHandler:
 
-    async def _send_bokeh_message(self, message: Message) -> int:
+    def __init__(self, websocket: Websocket):
+        self._socket = websocket
+
+    async def send_text(self, text):
+        await self._socket.send_text(text)
+        
+    async def send_bytes(self, bytestream):
+        await self._socket.send_bytestream(bytestream)
+
+    async def send_message(self, message: Message) -> int:
         sent = 0
         try:
             await self._socket.send_text(message.header_json)
@@ -145,23 +155,23 @@ class FastAPIServerConnection(ServerConnection):
             print(e)
         return sent
 
+        
 
-async def websocket_handler(socket: Websocket, application_context: ApplicationConext,
-                            token):
+async def websocket_handler(socket: Websocket, application_context: ApplicationConext, token):
     session_id = get_session_id(token)
     await application_context.create_session_if_needed(session_id, socket.scope, token)
     session = application_context.get_session(session_id)
 
+    ws_handler = SocketHandler(socket)
     protocol = Protocol()
     receiver = Receiver(protocol)
     handler = ProtocolHandler()
-    connection = FastAPIServerConnection(protocol, socket, application_context, session)
+    connection = ServerConnection(protocol, ws_handler, application_context, session)
 
     msg = connection.protocol.create('ACK')
-    await connection._send_bokeh_message(msg)
+    await ws_handler.send_message(msg)
 
     while True:
-
         ws_msg = await socket.receive()
 
         if 'text' in ws_msg:
@@ -170,24 +180,22 @@ async def websocket_handler(socket: Websocket, application_context: ApplicationC
         if message:
             work = await handler.handle(message, connection)
             if work:
-                await connection._send_bokeh_message(work)
-
-        import asyncio
-        await asyncio.sleep(0.1)
-
-
+                await ws_handler.send_message(work)
+        
 def _eval_panel(obj, doc: Document):
+    if isinstance(obj, (FunctionType, MethodType)):
+        obj = obj()
+        
     with set_curdoc(doc):
-        return as_panel(obj).server_doc(doc, location=False)
-
-
+        return as_panel(obj).server_doc(doc)
+   
 def add_application_routes(server, apps, prefix='/'):
     contexts = {}
-    for url, app in apps.items():
+    for url, app in apps.items():       
         if isinstance(app, pathlib.Path):
-            app = str(app)  # enables serving apps from Paths
+            app = str(app) # enables serving apps from Paths
         if (isinstance(app, str) and app.endswith(('.py', '.ipynb', '.md'))
-                and os.path.isfile(app)):
+            and os.path.isfile(app)):
             app = build_single_handler_application(app)
         elif not isinstance(app, Application):
             handler = FunctionHandler(partial(_eval_panel, app))
@@ -197,18 +205,21 @@ def add_application_routes(server, apps, prefix='/'):
 
     # Mount static file handlers
     for ext_name, ext_path in extension_dirs.items():
-        server.mount(f"/static/extensions/{ext_name}", StaticFiles(directory=ext_path),
-                     name=ext_name)
+        server.mount(f"/static/extensions/{ext_name}", StaticFiles(directory=ext_path), name=ext_name)
     server.mount("/static", StaticFiles(directory=settings.bokehjsdir()), name="static")
-
+        
     # Mount application router
     server.include_router(router.router)
-
 
 def create_server(apps):
     server = FastAPI()
     add_application_routes(server, apps)
     return server
 
+def panel_app():
+    import panel as pn
+    slider = pn.widgets.IntSlider(start=0, end=10, value=5)
+    out = pn.pane.Markdown('### ' + pn.rx('*') * slider)
+    return pn.template.MaterialTemplate(main=[slider, out], title='Hello World')
 
-app = create_server({'/foo': "# Hello World"})
+app = create_server({'/foo': panel_app})
